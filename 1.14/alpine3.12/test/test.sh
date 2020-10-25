1765be8e90a7b18347444f9bafa4e6c9b1e12ca2
charles-korpics <charles.korpics@ibm.com>
2020-10-25 17:14:31 -0400

    testing

---
+++ 1.14/alpine3.12/test/test.sh
@@ -0,0 +1,15 @@
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** testing docker run - golang *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW Download, install via go get: $ANSI_RESET"
+docker build . --tag local/golang:1.14
+docker run -i --rm --name some-golang local/golang:1.14 helloworld
+docker rmi local/golang:1.14
+
+echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
+
---
+++ 1.15/test-alpine/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW It can run a Java program: $ANSI_RESET"
+docker build 1.15/test/ runs-java-programs
+docker run --rm --name runs-java-programs test/run-app/quay.io/ibmz/openjdk
+docker rmi test/run-app/quay.io/ibmz/openjdk
+
+echo -e "\n $ANSI_GREEN *** FUNCTIONAL TEST(S) COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
