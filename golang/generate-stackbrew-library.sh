322afdb2eb0d953652d2d890a9cd164c6624dea4
Charles Korpics <Charles.Korpics@ibm.com>
2020-10-29 10:30:09 -0400

    Add files via upload

---
+++ golang/generate-stackbrew-library.sh
@@ -0,0 +1,174 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+declare -A aliases=(
+	[1.15]='1 latest'
+	[1.16-rc]='rc'
+)
+
+defaultDebianSuite='buster'
+declare -A debianSuite=(
+	#[1.13-rc]='buster'
+)
+defaultAlpineVersion='3.12'
+declare -A alpineVersion=(
+	#[1.9]='3.7'
+)
+
+self="$(basename "$BASH_SOURCE")"
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+if [ "$#" -eq 0 ]; then
+	versions="$(jq -r 'keys | map(@sh) | join(" ")' versions.json)"
+	eval "set -- $versions"
+fi
+
+# sort version numbers with highest first
+IFS=$'\n'; set -- $(sort -rV <<<"$*"); unset IFS
+
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		files="$(
+			git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++) {
+						if ($i ~ /^--from=/) {
+							next
+						}
+						print $i
+					}
+				}
+			'
+		)"
+		fileCommit Dockerfile $files
+	)
+}
+
+getArches() {
+	local repo="$1"; shift
+	local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
+
+	eval "declare -g -A parentRepoToArches=( $(
+		find -name 'Dockerfile' -exec awk '
+				toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
+					print "'"$officialImagesUrl"'" $2
+				}
+			' '{}' + \
+			| sort -u \
+			| xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
+	) )"
+}
+getArches 'golang'
+
+cat <<-EOH
+# this file is generated via https://github.com/docker-library/golang/blob/$(fileCommit "$self")/$self
+
+Maintainers: Tianon Gravi <admwiggin@gmail.com> (@tianon),
+             Joseph Ferguson <yosifkit@gmail.com> (@yosifkit),
+             Johan Euphrosine <proppy@google.com> (@proppy)
+GitRepo: https://github.com/docker-library/golang.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
+for version; do
+	export version
+	variants="$(jq -r '.[env.version].variants | map(@sh) | join(" ")' versions.json)"
+	eval "variants=( $variants )"
+
+	versionAliases=(
+		$version
+		${aliases[$version]:-}
+	)
+
+	for v in "${variants[@]}"; do
+		dir="$version/$v"
+		[ -f "$dir/Dockerfile" ] || continue
+
+		variant="$(basename "$v")"
+		versionSuite="${debianSuite[$version]:-$defaultDebianSuite}"
+
+		fullVersion="$(jq -r '.[env.version].version' versions.json)"
+
+		[[ "$fullVersion" == *.*[^0-9]* ]] || fullVersion+='.0'
+
+		if [ "$version" = "$fullVersion" ]; then
+			baseAliases=( "${versionAliases[@]}" )
+		else
+			baseAliases=( $fullVersion "${versionAliases[@]}" )
+		fi
+		variantAliases=( "${baseAliases[@]/%/-$variant}" )
+		variantAliases=( "${variantAliases[@]//latest-/}" )
+
+		if [ "${variant#alpine}" = "${alpineVersion[$version]:-$defaultAlpineVersion}" ]; then
+			variantAliases+=( "${baseAliases[@]/%/-alpine}" )
+			variantAliases=( "${variantAliases[@]//latest-/}" )
+		fi
+
+		case "$v" in
+			windows/*)
+				variantArches='windows-amd64'
+				;;
+
+			*)
+				variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
+				variantArches="${parentRepoToArches[$variantParent]}"
+
+				if [ "$variant" = 'stretch' ]; then
+					# stretch's "golang-go" package doesn't include GOARM for arm32v5
+					variantArches="$(sed <<<" $variantArches " -e 's/ arm32v5 / /g')"
+					# ... and "gccgo" in stretch can't build mips64le
+					variantArches="$(sed <<<" $variantArches " -e 's/ mips64le / /g')"
+				fi
+				;;
+		esac
+
+		sharedTags=()
+		for windowsShared in windowsservercore nanoserver; do
+			if [[ "$variant" == "$windowsShared"* ]]; then
+				sharedTags=( "${baseAliases[@]/%/-$windowsShared}" )
+				sharedTags=( "${sharedTags[@]//latest-/}" )
+				break
+			fi
+		done
+		if [ "$variant" = "$versionSuite" ] || [[ "$variant" == 'windowsservercore'* ]]; then
+			sharedTags+=( "${baseAliases[@]}" )
+		fi
+
+		constraints=
+		if [ "$variant" != "$v" ]; then
+			constraints="$variant"
+			if [[ "$variant" == nanoserver-* ]]; then
+				# nanoserver variants "COPY --from=...:...-windowsservercore-... ..."
+				constraints+=", windowsservercore-${variant#nanoserver-}"
+			fi
+		fi
+
+		commit="$(dirCommit "$dir")"
+
+		echo
+		echo "Tags: $(join ', ' "${variantAliases[@]}")"
+		if [ "${#sharedTags[@]}" -gt 0 ]; then
+			echo "SharedTags: $(join ', ' "${sharedTags[@]}")"
+		fi
+		cat <<-EOE
+			Architectures: $(join ', ' $variantArches)
+			GitCommit: $commit
+			Directory: $dir
+		EOE
+		[ -z "$constraints" ] || echo "Constraints: $constraints"
+	done
+done
